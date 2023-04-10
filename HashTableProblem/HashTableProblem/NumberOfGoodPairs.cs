2c38e0e5c5027b691771e690f819879c5864d97f
VovaHun <Djoblack313@yandex.ru>
2023-04-10 20:56:27 +0300

    Problem Number of good pairs

---
+++ HashTableProblem/HashTableProblem/NumberOfGoodPairs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableProblem
+{
+    internal class NumberOfGoodPairs
+    {
+        //if the array contains nums[i], then it increases mas[nums[i]] by one and increases res 
+
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int[] mas = new int[101];
+            int res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res += mas[nums[i]];
+                ++mas[nums[i]];
+            }
+            return res;
+
+
+
+            
+        }
+    }
+}
---
+++ HashTableProblem/HashTableProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using HashTableProblem;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        NumberOfGoodPairs numberOf = new NumberOfGoodPairs();
+
+        int[] mas1 = new int[] { 1, 2, 3, 1, 1, 3 };
+
+
+
+        int res1 =  numberOf.NumIdenticalPairs(mas1);
+
+        Console.WriteLine(res1);
+
+        Console.ReadKey();
+    }
+}
